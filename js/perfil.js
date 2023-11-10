2f2e592cb006cb4b25871243c0a857d59657c6f5
Luciano Castro <lfcastro2911@gmail.com>
2023-11-10 09:07:59 -0300

    validarNombre

--- js/perfil.js
+++ js/perfil.js
@@ -15,7 +15,7 @@ function cargarPerfil() {
   console.log("Cargando Usuarios... ");
 
   perfiles = Storage.cargar("perfiles") || [];
-
+  console.log("perfiles:", perfiles);
   player = Number(new URLSearchParams(window.location.search).get("player"));
 
   document.getElementById("jugador").innerHTML = player + 1;
@@ -31,49 +31,61 @@ function cargarPerfil() {
   }
 }
 
-function guardarPerfil(e) {
-  e.preventDefault();
+function guardarPerfil(event) {
+  event.preventDefault();
+  validarForm()
+  // ["nombre", "apodo", "color", "pic"].forEach((campo) =>
+  //   document.getElementById(campo)
+  // );
+  // Si el formulario es valido, se guarda en el Storage (base de datos)
+//   if (validarForm()) {
+//     console.log("Guardando..");
+//     perfiles[player] = {
+//       nombre: document.getElementById("nombre").value,
+//       apodo: document.getElementById("apodo").value,
+//       color: document.getElementById("color").value,
+//       pic: document.getElementById("pic").getAttribute("src"),
+//     };
 
-  ["nombre", "apodo", "color", "pic"].forEach((campo) =>
-    document.getElementById(campo).classList.remove("notValid")
-  );
+//     Storage.guardar("perfiles", perfiles);
 
-  if (validarForm()) {
-    perfiles[player] = {
-      nombre: document.getElementById("nombre").value,
-      apodo: document.getElementById("apodo").value,
-      color: document.getElementById("color").value,
-      pic: document.getElementById("pic").getAttribute("src"),
-    };
-
-    Storage.guardar("perfiles", perfiles);
-
-    if (perfiles.length < cantJugadores) {
-      window.location.href = "perfil.htm?player=" + perfiles.length;
-    } else {
-      window.location.href = "menu.html";
-    }
-  }
+//     if (perfiles.length < cantJugadores) {
+//       window.location.href = "perfil.htm?player=" + perfiles.length;
+//     } else {
+//       window.location.href = "menu.html";
+//     }
+//   }
 }
 
 function validarForm() {
-  if (!utils.validarCamposDeTexto(["nombre", "apodo", "color"], 2, esValido)) {
+  //   if (!utils.validarCamposDeTexto(["nombre", "apodo", "color"], 2, esValido)) {
+  //     return false;
+  //   }
+  let nombre = document.getElementById("nombre").value;
+//   let apodo = document.getElementById("apodo").value;
+//   let img = document.getElementById("pic").value;
+//   let color = document.getElementById("color").value;
+
+  if (nombre === "") {
+    console.log("Nombre vacio");
+    alert("El nombre esta vacio");
     return false;
   }
-  const img = document.getElementById("pic");
 
-  if (img.getAttribute("src") === "img/usuario-de-perfil.png") {
-    //todavía no se saco la foto y no es valido
+  
+ // apodo.length 
+  //   if (img.getAttribute("src") === "img/usuario-de-perfil.png") {
+  //     //todavía no se saco la foto y no es valido
 
-    img.classList.add("notValid");
+  //     img.classList.add("notValid");
 
-    img.focus();
+  //     img.focus();
 
-    return false;
-  } else {
-    img.classList.remove("notValid");
-  }
-  return true;
+  //     return false;
+  //   } else {
+  //     img.classList.remove("notValid");
+  //   }
+  //   return true;
 }
 
 function esValido(propiedad, valor) {
@@ -110,4 +122,6 @@ function onPictureError(err) {
 //     .getElementById("btn-guardar-perfil")
 //     .addEventListener("click", guardarPerfil, false);
 
-document.getElementById("btn-guardar-perfil").onclick = () => {};
+document.getElementById("btn-guardar-perfil").onclick = (event) => {
+  guardarPerfil(event);
+};
